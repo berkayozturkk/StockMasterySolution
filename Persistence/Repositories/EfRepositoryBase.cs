1ecbdebbe42f69b25ded9b4864863e04e08e951e
Berkay Öztürk <73599896+berkayozturkk@users.noreply.github.com>
2023-10-09 17:33:25 +0300

    Repository yapısının kurulması.

---
+++ Persistence/Repositories/EfRepositoryBase.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Repositories;
+
+public class EfRepositoryBase<TContext>
+{
+    protected readonly TContext _context;
+
+    public EfRepositoryBase(TContext context)
+    {
+        _context = context;
+    }
+}
---
+++ Persistence/Repositories/ItemRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Repositories;
+
+public class ItemRepository : EfRepositoryBase<BaseDbContext>
+{
+    public ItemRepository(BaseDbContext context) : base(context)
+    {
+
+    }
+
+    
+}
