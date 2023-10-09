11cd1ef364678cd32ecccf8d237f897a53a52866
Berkay Öztürk <73599896+berkayozturkk@users.noreply.github.com>
2023-10-09 16:33:05 +0300

    Creation of entities.

---
+++ Domain/Entities/Items.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities;
+
+public class Items
+{
+    public int ID { get; set; }
+    public int TransactionType { get; set; }
+    public string DocumentNo { get; set; }
+    public int Date { get; set; }
+    public string ProductCode { get; set; }
+    public int Amount { get; set; }
+    public int Price { get; set; }
+    public int Salary { get; set; }
+    public string Unit { get; set; }
+
+    public Items()
+    {
+        
+    }
+
+    public Items(int id, int transactionType, string documentNo, int date, string productCode, int amount, int price, int salary, string unit)
+    {
+        ID = id;
+        TransactionType = transactionType;
+        DocumentNo = documentNo;
+        Date = date;
+        ProductCode = productCode;
+        Amount = amount;
+        Price = price;
+        Salary = salary;
+        Unit = unit;
+    }
+}
---
+++ Domain/Entities/Products.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities;
+
+public class Products
+{
+    public int ID { get; set; }
+    public string ProductCode { get; set; }
+    public string ProductName { get; set; }
+
+    public Products()
+    {
+        
+    }
+
+    public Products(int id, string productCode, string productName)
+    {
+        ID = id;
+        ProductCode = productCode;
+        ProductName = productName;
+    }
+}
