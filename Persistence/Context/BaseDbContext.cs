4bf681d17163d4265a624528b884f4b4c185594d
Berkay Öztürk <73599896+berkayozturkk@users.noreply.github.com>
2023-10-09 17:06:13 +0300

    Creation of EntityFramework connections, registration of PersistenceServiceRegistration class.

---
+++ Persistence/Context/BaseDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Context;
+
+public class BaseDbContext : DbContext
+{
+    public IConfiguration Configuration { get; set; }
+
+    public DbSet<Item> Items { get; set; }
+    public DbSet<Product> Products { get; set; }
+
+    public BaseDbContext(DbContextOptions dbContextOptions, IConfiguration configuration) : base(dbContextOptions)
+    {
+        Configuration = configuration;
+        Database.EnsureCreated();
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
---
+++ Persistence/PersitsanceServiceRegistration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence;
+
+public static class PersitsanceServiceRegistration
+{
+    public static IServiceCollection AddPersistanceServices
+        (this IServiceCollection services, IConfiguration configuration)
+    {
+        //services.AddDbContext<BaseDbContext>(options => options.UseInMemoryDatabase("TESTDB12M"));
+        services.AddDbContext<BaseDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("TESTDB12M")));
+
+        return services;
+    }
+}
